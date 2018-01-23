8072c165327cdce123451c0aab68a7c6fd148984
heyong.wd <heyong.wd@alibaba-inc.com>
2018-01-23 11:42:45 +0800

    test

--- main/packages.go
+++ main/packages.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"fmt"
 )
 
@@ -23,8 +24,7 @@ var (
 )
 
 func main() {
-	fmt.Println(v1, p, v2, v3)
-
+	// fmt.Println(v1, p, v2, v3)
 	// fmt.Println("My favorite number is", rand.Intn(10))
 	// fmt.Println(add(1, 2))
 	// a, b := swap("hello", "world")
@@ -46,7 +46,7 @@ func main() {
 	// for i := 1; i <= 100; i++ {
 	// 	defer fmt.Println(i)
 	// }
-	// fmt.Println("done")
+	fmt.Println("done")
 
 	// i, j := 42, 2701
 
@@ -74,7 +74,9 @@ func main() {
 	// 	fmt.Println("d")
 	// }()
 	// panic("ex")
-
+	// x := "222"
+	// fmt.Printf(testFor(x))
+	// fmt.Printf(len(splitString(",")))
 }
 
 // func f() {
@@ -96,4 +98,10 @@ func split(sum int) (x, y int) {
 	x = sum * 6 / 2
 	y = sum - 1
 	return
+}
+func testFor(x string) string {
+	return x + "1"
+}
+func splitString(x string) []string {
+	return strings.Split("ak-code-phabricator,acc-acid-et2,acc-acs-et2,acc-asm-et2,acc-fs-et2,acc-fs-ivr-et2,acc-kaproxy-et2,acc-metaq,acc-rec-et2,acc-sipcap-et2,acc-statserver-et2,accs-dm,accs-sal,address-server,addresstokenizer,adp,air,aladdin,alc,ali360,alicarecore,alicom-recommend,alipay_notify,allcbu-notify,amdc,amp-message,anywhere,aqcenter,arcticmaster,arcticsmartisan,argus,artisan,authcenter,baoxianmisc,baoxianpricing,baoxianrecom,baoxianrisk,bbq,bicester,birdsnestcenter,buc-acl-hsf,buc-acl-hsf-dp,buc-authcenter,buc-sso,buc-uc-hsf,buy2,buycenter,cachechecker,capacitymanager,carcenter,carts,cartsapi,cartscache,cdc,cdc-dump,chanel,channelcenter,chongzhibiz,chongzhidata,chongzhidispatch,chongzhimisc,chongzhimobile,cnalgo,cncbf,cndps,cnortools-center,cnortools-task,coincore,collina,collinadecryptor,collinafacade,communityuis,connected,consign,consigncenter,corona-server,credit.center,crmcenter,crmroute,ctr,cuntaonetwork,customercenter.interface,cybertron,d4s,daa-traffic-hub,daa-traffic-op,delivery,detail,detailskip,diablo,diamond,dna,doraemon,dosa,durex,e56,easygocore,ecardcpgw,ecardmisccenter,falcon9r,fivemin,flow,fundgateway,fundplatform,geb,gebsupport,gemini,glaucus,globaladdressservice,gns,gsearch,guide,harbor,havanaauth,havanamlogin,headline,hlservice,iforest,industry-tagcenter,industry.center,inventoryhotel,inventoryplatform,iopen-core,itemcenter,itemtools,jiajucenter,jingweiconsole,ju,juathena,jucol,jucube,judetail,juhsf,juseller,langyan,lexus,liaoyuan,lightspeed,loc-vdump,login,logisticscenter,logisticsmarket-center,logisticsmarket-task,luna,macao,macross,malldetail,malldetailskip,mallitemcenter,marketing-center,maybach,mclaren,member-business-center,membercenter,memberplatform,metaq,metaq-ops,metaqtransfer,minerva-base,mix2notify,mix3notify,mix_notify,motormaster,mtask,mtee,mteemodel,mtop,mtopplatform,myju,namesrv,notify,offer_cbu,omni-industry-center,omni-sourcing,onepiece,onlinecs,openair,openbase,orderplatform,palisade,pamirsqogir,pamirssupport,pbservice,pesystem3,phenix-center,pointcenter,polaris,price-service,promotioncenter,promotiontag,protectcenter,qccenter,quicksilver,quotacenter,ratecenter,refundface,refundface2,refundplatform,refundplatform2,relationplatform,relationrecommend,renqun,resourcecenter,resourcecenterops,rmc,robot,robot-portal,s-account,saipan,saleenginesync,scskuconsole,seattle,security-tesla,securitymatrix,shellcenter,shopcenter,sic,sirius,site_service_center,sitesupport,smartisan,stationmobile,stationplatform,subaru,sync,taokeeper,tbusergw,tddl,tmallbuy,tmallequity,tmallincubator,tmallreport,top,topapi,toptim,track-service,trade_notify,trade_sub2_notify,trade_sub_notify,tradecenter,tradedata.center,trademanager,tradeplatform,trippe,trippeunit,triprule,tsplatform,ucc,uicfinal,uiclogin,uicplus,ultima,umid,ump,upp,usa,ussc,vipserver,volvo,wallet,warehouse-resource-center,warehouserouteservice,wdn,widgetpt,wormholesource,wsearch,wsm,wsnssync,wtt-channel-server,wtt-irs,wtt-trade-service,wtt-trade-web,wukong-amorite,wukong-fastproxy,wukong-file,wukong-im-processor,wukong-oauth,wukong-proxy,wukong-push-amorite,wukong-register,wukong-shaka,wukong-syncserver,xflush-cloud,xspace-account,xspace-bss,xspace-payment,xspace-scheduler,yosemite,hadesP0App", ",")
 }
